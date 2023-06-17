60eb87f6cb9688dd16b22908be46680ef4c0620e
= <=>
2023-06-17 22:18:09 +0530

    CG: Front Template Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GridComponent } from './grid/grid.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,9 +17,14 @@ import { GridComponent } from './grid/grid.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule, MatDividerModule, MatIconModule, BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+ }
--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.css']
+  styleUrls: ['./grid.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GridComponent implements OnInit {
   sudokugrid:any[]=[];
+  solved:boolean;
+  boxWidth="3.2"
+  validationError = false;
 
   constructor() {
+    this.solved = false;
     this.sudokugrid = [[0,0,0,0,0,0,0,0,0],
                        [0,0,0,0,0,0,0,0,0],
                        [0,0,0,0,0,0,0,0,0],
@@ -21,6 +26,29 @@ export class GridComponent implements OnInit {
    }
 
   ngOnInit(): void {
+  }
+
+  solveSudoku(){
+    console.log(this.sudokugrid)
+
+  }
+
+  getI(i:number,j:number){
+    return Math.floor(i/3)*3 + Math.floor(j/3);
+  }
+
+  getJ(i:number,j:number){
+    return(i%3)*3 + j%3;
+  }
+
+  validateInput(i:number,j:number){
+    if(Number(this.sudokugrid[i][j])<10 && Number(this.sudokugrid[i][j])>1){
+      this.validationError = false;
+    }
+    else{
+      this.validationError = true;
+    }
+
   }
 
 }
